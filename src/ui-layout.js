85ca6c7afb4cee540b28659c13d0665851a1af63
Grant Harris <grant@eatart.org>
2016-02-03 13:39:09 -0800

    quotes

--- src/ui-layout.js
+++ src/ui-layout.js
@@ -963,7 +963,7 @@ angular.module('ui.layout', [])
         };
       }])
 
-  .directive('uiLayoutLoaded', ['$timeout, Layout', function($timeout, Layout) {
+  .directive('uiLayoutLoaded', ['$timeout', 'Layout', function($timeout, Layout) {
     // Currently necessary for programmatic toggling to work with "initially" collapsed containers,
     // because prog. toggling depends on the logic of prevButton and nextButton (which should be probably refactored out)
     //
